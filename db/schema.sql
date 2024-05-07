02c2721020c6d97b166ffc25799d9b9710ee2f78
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-07 14:26:35 -0400

    Added department table creation script

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,7 @@ DROP DATABASE IF EXISTS employeecentral_db;
 CREATE DATABASE employeecentral_db;
 
 
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(30) INT NOT NULL
+);
