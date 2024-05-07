f0cd0e405f98d4300a1e6ffab8ee2b983a51a595
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-07 14:43:14 -0400

    Added the 'role' table schema with fields for job titles, salaries, and department assignments. Included a foreign key constraint for department references to maintain data integrity.

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,15 @@ CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name varchar(30) INT NOT NULL
 );
+
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2)
+  department_id INT
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
