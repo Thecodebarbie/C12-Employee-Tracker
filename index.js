c3c95aa79c66186410ec251d18bf5066ebc0b8f4
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-09 15:13:31 -0400

    format document

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const inquirer = require('inquirer')
-const {printTable} = require('console-table-printer')
+const { printTable } = require('console-table-printer')
 const mysql2 = require('mysql2')
 
 const db = mysql2.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'password',
-    database:'employee_db',
-    port:3306
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employee_db',
+    port: 3306
 })
 
-db.connect( ()=>{
+db.connect(() => {
     menu()
 })
 
 
-function menu(){
+function menu() {
     // view all deparrtments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
-inquirer.prompt([
-    {
-        type:'list',
-        message:'What would you like to do?',
-        name:'option',
-        choices:[ 'view all deparrtments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-    }
-])
-.then(response=>{
-    if(response.option === 'view all employees'){
-        viewAllEmployees()
-    }
-})
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'option',
+            choices: ['view all deparrtments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+        }
+    ])
+        .then(response => {
+            if (response.option === 'view all employees') {
+                viewAllEmployees()
+            }
+        })
 
 }
